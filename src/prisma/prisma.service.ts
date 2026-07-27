6f2c740f900c4e80b88ba18d49f115b15dc1a078
Jose Francisco Ortega Teran <joseortega9988@gmail.com>
2026-07-27 15:23:15 -0500

    added ssl reject only in Heroku production

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,7 +2,7 @@ import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { log } from 'console';
 import { PrismaPg } from '@prisma/adapter-pg';
-
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService 
@@ -10,14 +10,21 @@ export class PrismaService
     implements OnModuleInit, OnModuleDestroy
 {
     constructor () {
-        const adapter = new PrismaPg({
+        // 1. Check the environment
+        const isProduction = process.env.NODE_ENV === 'production';
+
+        // 2. Configure the connection pool with conditional SSL
+        const pool = new Pool({
             connectionString : process.env.DATABASE_URL,
-            ssl: { rejectUnauthorized: false },
+            ssl: isProduction ? { rejectUnauthorized: false } : undefined,
         });
+
+        // 3. Pass the pool to the Prisma adapter
+        const adapter = new PrismaPg(pool);
 
         super ({
             adapter,
-            log: process.env.NODE === 'development'
+            log: process.env.NODE_ENV === 'development'
             ? ['query', 'error', 'warn']
             : ['error']
         });
