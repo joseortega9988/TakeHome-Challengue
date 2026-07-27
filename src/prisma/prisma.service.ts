1b878fb46dfe313c313dd401e24c1dbb57dfa700
Jose Francisco Ortega Teran <joseortega9988@gmail.com>
2026-07-27 15:13:12 -0500

    added ssl reject auth becuase of documantation

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -12,6 +12,7 @@ export class PrismaService
     constructor () {
         const adapter = new PrismaPg({
             connectionString : process.env.DATABASE_URL,
+            ssl: { rejectUnauthorized: false },
         });
 
         super ({
